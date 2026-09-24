d7a31b0e535cbe037b6bf15eb2ed372234de2c89
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-24 17:14:36 +0300

    feat: bootstrap app with Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app/app.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const AppBootstrap());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class AppBootstrap extends StatelessWidget {
+  const AppBootstrap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return const ProviderScope(
+      child: SherkoPharmaApp(),
     );
   }
 }
