a44990d71b277cf7b82015505d676828d5776efd
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-24 17:14:40 +0300

    feat: add Riverpod navigation controller

---
+++ lib/features/navigation/application/app_navigation_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum AppDestination {
+  catalog,
+  order,
+}
+
+class AppNavigationController extends Notifier<AppDestination> {
+  @override
+  AppDestination build() {
+    return AppDestination.catalog;
+  }
+
+  void select(AppDestination destination) {
+    if (state == destination) {
+      return;
+    }
+
+    state = destination;
+  }
+}
+
+final appNavigationControllerProvider =
+    NotifierProvider<AppNavigationController, AppDestination>(
+      AppNavigationController.new,
+    );
