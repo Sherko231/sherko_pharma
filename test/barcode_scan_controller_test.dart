267a8e6e09527ee5103bd1db91f6ff33c5698fdb
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-26 16:17:03 +0300

    SP-013: complete duplicate-frame test setup

--- test/barcode_scan_controller_test.dart
+++ test/barcode_scan_controller_test.dart
@@ -62,7 +62,9 @@ void main() {
 
   test('concurrent duplicate frame is ignored', () async {
     final pending = Completer<List<CatalogProduct>>();
-    final catalog = FakeCatalogRepository()..onLookupBarcode = (_) => pending.future;
+    final catalog = FakeCatalogRepository()
+      ..onLookupBarcode = (_) => pending.future
+      ..onGet = (id) async => testProduct(id: id);
     final container = ProviderContainer();
     addTearDown(container.dispose);
     final scan = BarcodeScanController(
